557db2571486e8c608d447b8058dc2980125be32
OsimAkash <osimakash123@gmail.com>
2023-03-19 19:12:16 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// let a = prompt ("Hey whats you age")
+//  a = Number .parseInt(a); // Converting the string to number 
+//  if (a<0){
+//     alert("this is an invalid age");
+//  }
+// else if (a<9) {
+//     alert("You are a kid and you cannot even think of driving");
+// }
+// else if(a<16) {
+//     alert("You are a kid and think of driving after 18");
+// }
+// console.log("Done");
+// // HomeWork - Explore 
+
+// let age = 18;
+// let a = age > 18 ? "You can Drive" : "You cannot drive";
+// console.log (a)
